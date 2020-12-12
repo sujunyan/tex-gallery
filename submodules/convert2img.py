ff993a21c9dfbc98977157facc8d982ffc04fbe1
Junyan Su <sujy@shanghaitech.edu.cn>
2020-12-12 20:25:48 +0800

    change files to standalone format. added py to convert pdf to png

---
+++ submodules/convert2img.py
@@ -0,0 +1,16 @@
+from pdf2image import convert_from_path, convert_from_bytes
+import pathlib
+from pdf2image.exceptions import (
+    PDFInfoNotInstalledError,
+    PDFPageCountError,
+    PDFSyntaxError
+)
+
+
+curDir = pathlib.Path(__file__).parent.absolute()
+nameL = ["1","2a","2b","3a","3b"]
+for name in nameL:
+    images = convert_from_path(curDir.joinpath("{0}/{0}.pdf".format(name)))
+    images[0].save(curDir.joinpath("{0}/{0}.png".format(name)),"png")
+    print("{0}.pdf done".format(name))
+print("done")
