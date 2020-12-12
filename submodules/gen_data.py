264b406f7b5294ebfd05e218e6ff41df980d916e
Junyan Su <sujy@shanghaitech.edu.cn>
2020-12-12 18:13:12 +0800

    change the file structure

--- submodules/gen_data.py
+++ submodules/gen_data.py
@@ -4,7 +4,7 @@ import math
 import pathlib
 from math import log
 
-datafile = pathlib.Path(__file__).parent.absolute().joinpath("datafile.csv")
+datafile = pathlib.Path(__file__).parent.absolute().joinpath("datafile1.csv")
 with open(datafile,"w",newline="") as f:
     f_csv = csv.writer(f,delimiter=" ") 
     xv = np.linspace(0,120)
